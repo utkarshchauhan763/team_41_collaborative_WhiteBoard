1911467d0a47fe69b8e9efe21b60bfe3a3d6ae27
Utkarsh Chauhan <142593247+utkarshchauhan763@users.noreply.github.com>
2026-05-05 15:49:45 +0530

    Delete backend/src/main/java/com/team41/whiteboard/persistence directory

--- backend/src/main/java/com/team41/whiteboard/persistence/SessionMetadataRepository.java
+++
@@ -1,6 +0,0 @@
-package com.team41.whiteboard.persistence;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SessionMetadataRepository extends JpaRepository<SessionMetadataEntity, String> {
-}
